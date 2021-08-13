b034ee75c3d9d24a4689dce546c2d37288ae93ff
jhye0n <stjhyeon@kakao.com>
2021-08-14 05:14:02 +0900

    [REMOVE] older folder

--- File Manager/src/flag.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(!preg_match("/".$_SERVER['HTTP_HOST']."/i",$_SERVER['HTTP_REFERER'])){
-    die("No ^~^");
-}else{
-    $flag = "HCAMP{this_3s_file_manager_exploit!!}";
-    die($flag);
-}

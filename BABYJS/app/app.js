8dd7e493fb28ee321a0492460d8f0852bd1ad9ce
jhye0n <stjhyeon@kakao.com>
2021-08-13 21:37:54 +0900

    [Update] babyjs code patch

--- BABYJS/app/app.js
+++ BABYJS/app/app.js
@@ -15,13 +15,15 @@ app.get('/', function(req, res, next){
     if(code){
         try {
             const result = vm.runInNewContext(`'use strict'; ${code}`, {}, { timeout: 100 });
+            if(result && code){
+                msg = "nice try!";
+            }
         } catch(e) {
             msg = "err";
         }
     } else {
             msg = "err";
     }
-    
     res.render("index", {"title": "Hello BABYJS", msg});
 });
 
